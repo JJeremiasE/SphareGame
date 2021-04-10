199f1cf249a8fc801e5c0b553eaf2b4da7ccfbd2
JJeremiasE <j.j.e@hotmail.fi>
2021-04-10 21:17:10 +0300

    MainFile
    Tähän asti tehdyt asiat

---
+++ SphareGame/Assets/Scripts/FailState.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FailState : MonoBehaviour
+{
+         private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            print("YOU LOSE");
+
+            other.transform.position = new Vector3( 0, 0, 14);
+            other.GetComponent<Rigidbody>().velocity = Vector3.zero; //(0,0,0)
+
+        }
+    }
+}
